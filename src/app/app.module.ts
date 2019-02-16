719d6a05ccc540d86881423db7dd37094100859a
bhandariyash <bhandariyash94@gmail.com>
2019-02-16 14:45:41 +0530

    added and modified
    added new files and made modifications in existing files where required.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { EmployeeComponent } from './employee/employee.component';
+import { HospitalInfoComponent } from './hospital-info/hospital-info.component';
+import { InsuranceProviderComponent } from './hospital-info/insurance-provider/insurance-provider.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    EmployeeComponent,
+    HospitalInfoComponent,
+    InsuranceProviderComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/employee/employee.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-employee',
+  templateUrl: './employee.component.html',
+  styleUrls: ['./employee.component.css']
+})
+export class EmployeeComponent implements OnInit {
+  name: string = 'Yash';
+  city: string = '';
+  isHidden: boolean = false;
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  getName() : string {
+    return this.name;
+  }
+
+  changeName(name: any) {
+    //console.log(name.target.value);
+    this.name = name.target.value;
+    this.city = "Pune";
+  }
+
+  toggleInputs() {
+    this.isHidden = !this.isHidden;
+  }
+}
